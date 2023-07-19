65d1506f118a06a896591acabcd78e3c62e7b834
calvin3oo <calvinzboss@gmail.com>
2023-07-18 22:13:01 -0500

    fixed bug with days since last visit

--- chamber/scripts/daysSinceLastVisit.js
+++ chamber/scripts/daysSinceLastVisit.js
@@ -1,13 +1,15 @@
 if (typeof(Storage) !== "undefined") {
-var currentTimestamp = Date.now();
+    var currentTimestamp = Date.now();
 
-var previousTimestamp = localStorage.getItem("lastVisitTimestamp");
+    var previousTimestamp = localStorage.getItem("lastVisitTimestamp");
 
-if (previousTimestamp) {
-    var timeDifference = Math.round((currentTimestamp - previousTimestamp) / (1000 * 60 * 60 * 24));
+    if (previousTimestamp) {
+        var timeDifference = Math.round((currentTimestamp - previousTimestamp) / (1000 * 60 * 60 * 24));
 
-    document.getElementById("timeDifference").textContent = "Days since last visit: " + timeDifference;
-}
+        document.getElementById("timeDifference").textContent = "Days since last visit: " + timeDifference;
+    } else {
+        document.getElementById("timeDifference").textContent = "Days since last visit: 0";
+    }
 
-localStorage.setItem("lastVisitTimestamp", currentTimestamp);
+    localStorage.setItem("lastVisitTimestamp", currentTimestamp);
 }
