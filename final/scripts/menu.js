a98175132f1ca8ec9652e9534d4d12b6ef2d1484
calvin3oo <calvinzboss@gmail.com>
2024-04-08 11:22:53 -0600

    progress on final home page

---
+++ final/scripts/menu.js
@@ -0,0 +1,37 @@
+// Adds the Menu HTML to the page along with a 'active' class to the 'active' tab
+function addMenu() {
+  const menuElement = document.getElementById("menu");
+  const navigation = [
+    { name: "Home", url: "index.html" },
+    { name: "Rentals", url: "rentals.html" },
+    { name: "Reservations", url: "reservations.html" },
+    { name: "Contact", url: "contact.html" },
+  ];
+  navigation.forEach((nav) => {
+    const liElement = document.createElement("li");
+    const aElement = document.createElement("a");
+    aElement.textContent = nav.name;
+    aElement.href = nav.url;
+
+    if (window.location.pathname.includes(nav.url)) {
+      aElement.classList.add("active");
+    }
+
+    liElement.appendChild(aElement);
+    menuElement.appendChild(liElement);
+  });
+}
+
+// Adds expanding and collapsing capability to the hamburger menu
+function addHamburgerListener() {
+  document.getElementById("hamburger").addEventListener("click", (e) => {
+    // toggle the hamburger
+    e.target.classList.toggle("open");
+
+    // show the menu
+    document.getElementById("menu").classList.toggle("mobile-hidden");
+  });
+}
+
+addMenu();
+addHamburgerListener();
